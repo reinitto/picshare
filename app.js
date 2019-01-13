0b17e578e9b544c40a3119eb04dd3acf225e8727
Labais <reinitto@gmail.com>
2019-01-13 17:01:48 +0100

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const resizeImg = require("resize-img");
-const keys = require("./config/keys");
+//const keys = require("./config/keys");
 const Schema = mongoose.Schema;
 //Init Multer Storage
 const storage = multer.diskStorage({
