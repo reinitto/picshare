2eaf494067b17503b9bd970ce2522fa8ddadefbc
Labais <reinitto@gmail.com>
2019-01-13 17:30:37 +0100

    add

--- app.js
+++ app.js
@@ -337,12 +337,15 @@ function resizeImages(array) {
       width: 512,
       height: 512
     }).then(buf => {
-      fs.writeFileSync(__dirname + `/uploads/small/${file.originalname}`, buf);
+      fs.writeFileSync(
+        __dirname + `public/uploads/small/${file.originalname}`,
+        buf
+      );
       // encode the file as a base64 string.
       let newPic = new Image({
         name: file.originalname,
         data: fs.readFileSync(
-          __dirname + `./uploads/small/${file.originalname}`
+          __dirname + `public/uploads/small/${file.originalname}`
         ),
         type: file.contentType,
         comments: []
